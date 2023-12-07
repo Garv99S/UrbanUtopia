e0d3bde8c9091a50efa70e39d260ca67d4af8c89
Gaurav Singla <gauravsingla1012@gmail.com>
2023-12-07 14:31:30 +0530

    complete show user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,8 @@ export default function Profile() {
   const [formData,setFormData] = useState({});
   const [updateSuccess,setUpdateSuccess] = useState(false);
   const dispatch = useDispatch();
+  const [showListingsError,setShowListingsError] = useState(false);
+  const [userListings,setUserListings] = useState([]);
 
 
   // firebase storage
@@ -110,6 +112,21 @@ export default function Profile() {
     }
   }
 
+  const handleShowListings = async() => {
+    try{
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if(data.success === false){
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    }catch(err){
+      setShowListingsError(true);
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7"> Profile </h1>
@@ -168,20 +185,75 @@ export default function Profile() {
           className="border p-3 rounded-lg"
           onChange={handleChange}
         ></input>
-        <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
-          {loading ? 'loading...' : 'Update'}
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "loading..." : "Update"}
         </button>
-        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
-            Create Listing
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >
+          Create Listing
         </Link>
-
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer font-semibold " onClick={handleDeleteUser}>Delete Account</span>
-        <span className="text-red-700 cursor-pointer font-semibold " onClick={handleSignOut}>Sign Out</span>
+        <span
+          className="text-red-700 cursor-pointer font-semibold "
+          onClick={handleDeleteUser}
+        >
+          Delete Account
+        </span>
+        <span
+          className="text-red-700 cursor-pointer font-semibold "
+          onClick={handleSignOut}
+        >
+          Sign Out
+        </span>
       </div>
-      <p className="text-red-700 mt-5"> {error ? error : ' '} </p>
-      <p className="text-green-700 mt-5"> {updateSuccess ? 'User is updated Successfully!!' : ' '} </p>
+      <p className="text-red-700 mt-5"> {error ? error : " "} </p>
+      <p className="text-green-700 mt-5">
+        {" "}
+        {updateSuccess ? "User is updated Successfully!!" : " "}{" "}
+      </p>
+      <button onClick={handleShowListings} className="text-green-700 w-full">
+        Show Listings
+      </button>
+      <p className="text-red-700 mt-5">
+        {showListingsError ? "Error showing listings" : ""}
+      </p>
+
+      {userListings && userListings.length > 0 &&
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
+          {userListings.map((listing) => (
+          <div
+            key={listing._id}
+            className="border rounded-lg p-3 flex justify-between items-center gap-4"
+          >
+            <Link to={`/listing/${listing._id}`}>
+              <img
+                className="h-16 w-16 object-contain"
+                src={listing.imageUrls[0]}
+                alt="listing cover"
+              />
+            </Link>
+            <Link
+              className="text-slate-700 font-semibold hover:underline trunctate flex-1"
+              to={`/listing/${listing._id}`}
+            >
+              <p>{listing.name}</p>
+            </Link>
+
+            <div className="flex flex-col items-center">
+              <button className="text-red-700 uppercase">Delete</button>
+              <button className="text-green-700 uppercase">Edit</button>
+            </div>
+          </div>
+          ))}
+        </div>
+      }
     </div>
   );
 }
