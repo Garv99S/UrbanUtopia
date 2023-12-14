6301e6aff73a2542f4d85c05c3422b9dbce6ce8a
Gaurav Singla <gauravsingla1012@gmail.com>
2023-12-14 08:32:32 +0530

    complete contact component and create search api route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-slate-500">Urban</span>
-            <span className="text-slate-500">Utopia</span>
+            <span className="text-slate-700">Utopia</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
